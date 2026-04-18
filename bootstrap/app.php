4ecd44856538e774499ab8d8aff2dd058dd16ad7
Obed <hedonobed555@gmail.com>
2026-04-18 16:19:51 +0200

    Fix: Force config path for Laravel 11 on Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,8 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+    // On force Laravel à trouver le bon dossier de configuration
+    ->withConfigPath(dirname(__DIR__).'/config') 
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
